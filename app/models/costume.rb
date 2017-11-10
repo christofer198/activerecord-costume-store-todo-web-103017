c816e5cf3a3fc46c06151ab75a2269ef1ac6e55a
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-10 21:13:35 +0000

    Done.

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,2 +1,14 @@
 # Create your Costume class here
 # It should inherit from ActiveRecord::Base
+class Costume < ActiveRecord::Base
+
+  # def created_at
+  #   Time.now
+  # end
+  #
+  # def updated_at
+  #   now = Time.now
+  #   Location.where('updated_at > now')
+  # end
+
+end
