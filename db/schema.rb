9b6e2fce1c194fd24362cd5e43dfe61e57f5a1e0
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-10 20:53:42 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -34,6 +34,9 @@ ActiveRecord::Schema.define(version: 3) do
   end
 
   create_table "haunted_houses", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.string "theme"
   end
 
 end
