5750dbff526c28e2bb26ffdafa31367ad2ec8f28
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-10 20:44:03 +0000

    automatically backed up by learn

--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -1,13 +1,4 @@
 # Create your CostumeStore class here
 class CostumeStore < ActiveRecord::Base
-  
+
 end
-# create_table :costume_stores do |t|
-#   t.string :name
-#   t.string :location
-#   t.integer :costume inventory
-#   t.integer :number_of_employees
-#   t.boolean :open
-#   t.datetime :opening
-#   t.datetime :closing
-# end
