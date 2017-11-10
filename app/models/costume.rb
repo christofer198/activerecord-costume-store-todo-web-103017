8d43b8251c5e302cbceab89d9ea16c0077cf9c83
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-10 21:13:25 +0000

    automatically backed up by learn

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -2,13 +2,13 @@
 # It should inherit from ActiveRecord::Base
 class Costume < ActiveRecord::Base
 
-  def created_at
-    Time.now
-  end
-
-  def updated_at
-    now = Time.now
-    Location.where('updated_at > now')
-  end
+  # def created_at
+  #   Time.now
+  # end
+  #
+  # def updated_at
+  #   now = Time.now
+  #   Location.where('updated_at > now')
+  # end
 
 end
