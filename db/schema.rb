cfc8066bfbaf22c96dab5d507c9315ffc9af13e0
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-10 20:55:34 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -37,6 +37,11 @@ ActiveRecord::Schema.define(version: 3) do
     t.string "name"
     t.string "location"
     t.string "theme"
+    t.integer "price"
+    t.boolean "family_friendly"
+    t.string "opening_date"
+    t.string "closing_date"
+    t.string "description"
   end
 
 end
